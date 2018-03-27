310b236ed9a232fdb39e0b29f14dd4656b452d4e
Pedro H. B. Cavalcante <pedrohbcavalcante@ufrn.edu.br>
2018-03-26 21:30:32 -0300

    Não obtive sucesso
    Não consegui criar o contador utilizando .post ou .postDelayed

--- app/src/main/java/com/example/pedro/tarefa4/Questao1.java
+++ app/src/main/java/com/example/pedro/tarefa4/Questao1.java
@@ -1,14 +1,54 @@
 package com.example.pedro.tarefa4;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static java.lang.Boolean.FALSE;
 
 public class Questao1 extends Activity{
+
+    private Button btnProcessar;
+    private TextView contador;
+    private Handler contagem = new Handler();
+    private int i = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questao1);
+
+
+        contador = findViewById(R.id.id_contador);
+        btnProcessar = findViewById(R.id.processar);
+    }
+
+    public void processar(View view){
+        btnProcessar.setEnabled(false);
+        executarContagem();
+    }
+
+    private void executarContagem(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                    contagem.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            for(i = 10; i >= 0; i--)
+                                SystemClock.sleep(100);
+                                contador.setText(String.valueOf(i));
+                        }
+                    });
+
+            }
+        }).start();
     }
 }
